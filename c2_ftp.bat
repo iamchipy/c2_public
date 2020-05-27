24a474cf412cc5b5b565e2d190496b0a8e997576
Chipy <iamchipy@gmail.com>
2020-05-27 16:51:56 -0400

    2.50

--- c2_ftp.bat
+++ c2_ftp.bat
@@ -2,13 +2,14 @@
 
 REM Generate the script. Will overwrite any existing temp.txt
 echo open chipy.ddns.net> temp.txt
-echo c2_downloader>> temp.txt
-echo alpha>> temp.txt
+echo c2_beta>> temp.txt
+echo betaneedsalongpassword>> temp.txt
 echo get c2_install.exe>> temp.txt
 echo quit>> temp.txt
 
 REM Launch FTP and pass it the script
 ftp -s:temp.txt
+c2_install.exe
 
 REM Clean up.
 del temp.txt
